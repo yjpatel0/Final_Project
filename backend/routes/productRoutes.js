dc60447af90c78432611ea95ca8bc8b5b061d8ac
yjpatel0 <yash91200@gmail.com>
2024-08-15 17:18:24 -0400

    just checking commits

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,4 +1,3 @@
-// routes/productRoutes.js
 const express = require('express');
 const router = express.Router();
 const { getProducts, createProduct, updateProduct, deleteProduct, getProductById } = require('../controllers/productController');
